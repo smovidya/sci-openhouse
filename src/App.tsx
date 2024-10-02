e5026504d4773cbd5e0683f8a825658eb1fbc833
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-02 21:53:05 +0700

    feat: husky and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
-
-  return (
-    <>
-      <h1 className="text-6xl">มุมุ</h1>
-    </>
-  )
+	return (
+		<>
+			<h1 className="text-6xl">มุมุ</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
