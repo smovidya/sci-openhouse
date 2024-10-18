a8a84d5a3ad0f25de27be7c4742acd2756bfc560
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-10-19 00:31:42 +0700

    fix/assets color and rm displaysvg

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Home } from './page/home.tsx';
+
 //หน้า render หลัก
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<Home/>
-import DisplaySVG from './displaySVG.tsx';
-
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-		<DisplaySVG />
-	</StrictMode>
+    <Home />
 );
