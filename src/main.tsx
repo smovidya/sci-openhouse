a434af05798ba4a49e39d0bdee45477d762a04a4
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-10-19 21:30:19 +0700

    add background

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Home } from './page/home.tsx';
-
 //หน้า render หลัก
 createRoot(document.getElementById('root')!).render(
         <Home />
