8d24b65bf11609208db7f982efe5aff0beadddf5
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2024-10-20 20:47:22 +0700

    Home and Login page

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -10,15 +10,15 @@ const Login = () => {
 	const [name, setName] = useState(''); //เก็บชื่อ
 	const handleNext = (name: string) => {
 		if (name != '') {
-			console.log(name);
+			console.log({name:name});
 			setNext(false);
 		}
 	};
 	return (
-		<div className="absolute left-1/2 transform -translate-x-1/2 flex min-h-screen w-full max-w-md">
+		<div className="flex justify-center items-center w-full max-w-md mx-auto">
 			{next ? (
-				<div className="h-screen w-screen bg-[url('../assets/defaultBG.svg')] flex flex-col items-center justify-between px-[40px] py-[47px]">
-					<div className="w-full h-full p-[18px] rounded-[20px] border-4 border-[#f9cd2f] flex flex-col gap-[28px] items-center">
+				<div className="w-full min-h-screen bg-cover overflow-hidden relative px-[40px] py-[47px] bg-[url('../assets/LoginBG.svg')] rounded-[20px] flex flex-col justify-start items-center gap-[38px]">
+					<div className="w-full h-full p-[18px] flex flex-col gap-[28px] items-center">
 						<img
 							className="w-20 h-20"
 							src="../assets/logo white-border-yellow.svg"
@@ -27,7 +27,7 @@ const Login = () => {
 							<Header />
 						</div>
 						<div className="w-full">
-							<div className="w-full text-center text-2xl text-[#f9cd2f] font-ibm-plex-thai mb-4">
+							<div className="w-full text-center text-2xl text-Yellow font-ibm-plex-thai mb-4">
 								DROP YOUR NAME
 							</div>
 							<Input
@@ -41,8 +41,7 @@ const Login = () => {
 								<Button
 									onClick={() => handleNext(name)}
 									type="submit"
-									variant="outline"
-									className="h-10 px-6 py-2 rounded-md bg-[#f9cd2f] text-black font-medium"
+									className="h-10 px-6 py-2 rounded-md bg-Yellow text-black font-medium"
 								>
 									Next
 								</Button>
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -16,43 +16,33 @@ const Home = () => {
 	};
 
 	return (
-		<div className="absolute left-1/2 transform -translate-x-1/2 flex min-h-screen w-full max-w-md">
+		<div
+			className="flex justify-center items-center w-full max-w-md mx-auto"
+		>
 			{home ? (
 				<div
-					className={`absolute inset-0 flex items-center justify-center transition-all duration-500 
-		${animationEnd ? 'opacity-0 -translate-y-10' : 'opacity-100 translate-y-0'}`}
+					className={`w-full h-full flex items-center justify-center transition-all duration-500 
+        ${animationEnd ? 'opacity-0 -translate-y-10' : 'opacity-100 translate-y-0'}`}
 					onClick={handleHome}
 				>
-					<div className="w-full h-screen bg-cover overflow-hidden relative px-[40px] py-[47px] bg-[url('../assets/defaultBG.svg')] rounded-[20px] flex-col justify-start items-center gap-[38px] inline-flex">
-						<div className="relative w-full h-full p-[18px] rounded-[20px] border-4 border-[#f9cd2f] flex-col justify-start items-center gap-[38px] inline-flex">
-							<img
-								src="../assets/moon.svg"
-								className="absolute top-[-100px] right-[-120px] -rotate-45 scale-150 z-10"
-							/>
-
-							<img
-								className="w-20 h-20"
-								src="../assets/logo white-border-yellow.svg"
-							/>
-							<div className="h-48 p-5 rounded-md flex-col justify-center items-center gap-[18px] flex">
-								<HeaderHome />
-							</div>
-							<div className="text-center text-[#f9cd2f] text-xl font-SilverStone-Regular uppercase leading-7">
-								Click to continue
-							</div>
-							<Sponser />
-
-							<img
-								src="../assets/butterfly2.svg"
-								className="absolute bottom-[-120px] left-[-150px] rotate-45 z-10 scale-125"
-							/>
+					<div className="w-full min-h-screen bg-cover overflow-hidden relative px-[40px] py-[47px] bg-[url('../assets/HomeBG.svg')] rounded-[20px] flex flex-col justify-start items-center gap-[38px]">
+						<img
+							className="w-20 h-20"
+							src="../assets/logo white-border-yellow.svg"
+						/>
+						<div className="h-48 p-5 rounded-md flex flex-col justify-center items-center gap-[18px]">
+							<HeaderHome />
 						</div>
+						<div className="text-center text-Yellow text-xl font-SilverStone-Regular uppercase leading-7">
+							Click to continue
+						</div>
+						<Sponser />
 					</div>
 				</div>
 			) : (
 				<div
-					className={`absolute inset-0 flex items-center justify-center transition-all duration-500 
-	${!home ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+					className={`w-full h-full flex items-center justify-center transition-all duration-500 
+        ${!home ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
 				>
 					<Login />
 				</div>
