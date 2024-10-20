6e5fc06f55b1ca2e5d494ae603a94121eba7b9ca
Nathawat Wai <nathawat12246@gmail.com>
2024-10-20 19:09:41 +0700

    fix format

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from "path"
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "../.."),
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, '../..'),
+		},
+	},
+});
