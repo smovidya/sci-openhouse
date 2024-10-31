b0b8170ba446aec5c108a63bad7bd3d20995b0fb
Nathawat Chan-iat <151960821+OverIoad@users.noreply.github.com>
2024-10-31 15:31:03 +0700

    Update home.tsx
    change duration to 0.5

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -19,7 +19,7 @@ const Home = () => {
 	  initial={{ opacity: 0 }} 
       animate={{ opacity: 1 }}  
       exit={{ opacity: 0 }}     
-      transition={{ duration: 2 }} 
+      transition={{ duration: 0.5 }} 
       onClick={handleLogin}
     >
       <img
